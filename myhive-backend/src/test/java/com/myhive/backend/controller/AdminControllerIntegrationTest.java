4433fffc159e288fd4e6266ac3bd36618f3fe417
Samir.Maksimov <samir.maximov@gmail.com>
2026-04-28 21:44:56 +0200

    refactor: replace wildcard MockMvcRequestBuilders import with explicit imports

--- myhive-backend/src/test/java/com/myhive/backend/controller/AdminControllerIntegrationTest.java
+++ myhive-backend/src/test/java/com/myhive/backend/controller/AdminControllerIntegrationTest.java
@@ -24,7 +24,10 @@ import static com.myhive.backend.util.JwtTestHelper.adminJwt;
 import static com.myhive.backend.util.JwtTestHelper.managerJwt;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
